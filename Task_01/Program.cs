741dfbb02a4145e24e465b2eb8d04df13bf0570d
Dmytro Pylypchuk <pylypchukd@gmail.com>
2022-11-03 18:30:25 +0200

    Add project files.

---
+++ Task_01/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Создайте класс, который позволит выполнять мониторинг ресурсов, используемых программой. 
+             * Используйте его в целях наблюдения за работой программы, а именно: 
+             * пользователь может указать приемлемые уровни потребления ресурсов (памяти), а методы класса позволят выдать предупреждение, 
+             * когда количество реально используемых ресурсов приблизиться к максимально допустимому уровню. 
+             */
+
+            Console.Write("Укажите максимальный размер памяти в Кб для вывода сообщения: ");
+            int.TryParse(Console.ReadLine(), out int memoryItem);
+
+            Thread thread = new Thread(MyMethod);
+            thread.Start();
+
+            while (true)
+            {
+                var totalMemory = GC.GetTotalMemory(false) / 1024;
+                if (totalMemory > memoryItem)
+                {
+                    Console.WriteLine("Ресурсы на исходе!");
+                    thread.Abort();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Потребление памяти {0} KB", totalMemory);
+                    Thread.Sleep(100);
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+
+        static void MyMethod()
+        {
+            OtherObject[] objects = new OtherObject[10000];
+
+            for (int i = 0; i < objects.Length; i++)
+            {
+                objects[i] = new OtherObject();
+
+                Thread.Sleep(10);
+            }
+        }
+    }
+
+    class OtherObject
+    {
+        byte[] array = new byte[1024 * 10];
+    }
+
+
+
+
+}
---
+++ Task_02/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            * Создайте свой класс, объекты которого будут занимать много места в памяти (например, в коде класса будет присутствовать большой массив) и реализуйте для этого класса формализованный шаблон очистки.
+            */
+
+            MyClass myClass = new MyClass();
+            Console.WriteLine($"Поколение объекта {GC.GetGeneration(myClass)}");
+
+            // Delay.
+            Console.ReadKey();
+
+        }
+
+        class MyClass : IDisposable
+        {
+            Array myArray = new int[100_000_000];
+            public MyClass()
+            {
+                Console.WriteLine(this.GetHashCode());
+            }
+            ~MyClass()
+            {
+                Dispose(false);
+                Console.WriteLine("Объект " + this.GetHashCode() + " удален");
+            }
+
+            private bool disposed = false;
+
+            public void Dispose()
+            {
+                Dispose(true);
+                GC.SuppressFinalize(this);
+            }
+            protected virtual void Dispose(bool disposing)
+            {
+                if (disposed) return;
+                
+                if (disposing)
+                {
+                    
+                }
+                disposed = true;
+            }
+        }
+    }
+}
